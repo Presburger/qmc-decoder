d41efe3f26a12584cddd32c9c22f815768f48c8e
presburger <mayushengmusic@gmail.com>
2019-04-07 21:43:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+#QMC-DECODER
+
+转化QQ Music QMC to MP3 or FLAC
+
+* build
+
+```
+mkdir build
+cd build
+cmake ..
+make 
+```
+
+* convert
+
+```
+cd build
+./decoder <file1> <file2> ..... 
+```
+
+* batch convert
+
+```
+find test -type f -name "*.qmcflac" -print0 | xargs -0 ./decoder
+```
---
+++ src/decoder.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+class Seed{
+    public:
+        Seed():x(-1),y(8),dx(1),index(-1){
+            seedMap={
+                {0x4a, 0xd6, 0xca, 0x90, 0x67, 0xf7, 0x52},
+                {0x5e, 0x95, 0x23, 0x9f, 0x13, 0x11, 0x7e},
+                {0x47, 0x74, 0x3d, 0x90, 0xaa, 0x3f, 0x51},
+                {0xc6, 0x09, 0xd5, 0x9f, 0xfa, 0x66, 0xf9},
+                {0xf3, 0xd6, 0xa1, 0x90, 0xa0, 0xf7, 0xf0},
+                {0x1d, 0x95, 0xde, 0x9f, 0x84, 0x11, 0xf4},
+                {0x0e, 0x74, 0xbb, 0x90, 0xbc, 0x3f, 0x92},
+                {0x00, 0x09, 0x5b, 0x9f, 0x62, 0x66, 0xa1}
+            };
+        }
+
+        uint8_t NextMask(){
+            uint8_t ret;
+            index++;
+            if(x<0)
+            {
+                dx = 1;
+                y = (8-y)%8;
+                ret = (8 -y)%8;
+                ret = 0xc3;
+            }else if(x > 6){
+                dx = -1;
+                y= 7-y;
+                ret = 0xd8;
+            }else{
+                ret = seedMap[y][x];
+            }
+
+            x += dx;
+            if(index==0x8000||(index>0x8000&&(index+1)%0x8000==0))
+                return NextMask();
+            return ret;
+        }
+
+
+    private:
+
+        int x;
+        int y;
+        int dx;
+        int index;
+        vector<vector<uint8_t> > seedMap;
+};
+
+
+void process(string dir)
+{
+    std::cout<<"decode "<<dir<<std::endl;
+    std::fstream infile(dir.c_str(),std::ios::in|std::ios::binary);
+    assert(infile);
+
+
+    string outloc(std::move(dir));
+
+    string bak;
+    char x;
+    while((x=outloc.back())!='.')
+    {
+        bak.push_back(x);
+        outloc.pop_back();
+    }
+
+    if(bak=="calf"||bak=="3p3")
+        return;
+
+    assert(bak.size()>3);
+    for(int u=0;u<3;++u)
+        bak.pop_back();
+    std::reverse(bak.begin(),bak.end());
+    outloc+=bak;
+
+
+    auto len = infile.seekg(0,std::ios::end).tellg();
+    infile.seekg(0,ios::beg);
+    char * buffer =new char[len];
+
+    infile.read(buffer,len);
+    infile.close();
+
+    Seed seed;
+    for(int i=0;i<len;++i)
+    {
+        buffer[i]=seed.NextMask()^buffer[i];
+    }
+
+    std::fstream outfile(outloc.c_str(),ios::out|ios::binary);
+    assert(outfile);
+    outfile.write(buffer,len);
+    outfile.close();
+
+
+}
+
+int main(int argc,char ** argv){
+
+    if(argc<2)
+    {
+        std::cout<<"./decoder qmcfile"<<std::endl;
+        return 1;
+    }
+
+    for(int i=1;i<argc;++i)
+        process(std::string(argv[i]));
+
+    return 0;
+}
