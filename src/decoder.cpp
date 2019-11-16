0e8a838b1169b22b4ae1d486be72594deffe073c
Presburger <mayushengmusic@gmail.com>
2019-11-16 15:57:17 +0800

    support qmcogg,and make code better

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -1,6 +1,5 @@
-#include "seed.hpp"
 #include <cassert>
-#include <boost/filesystem.hpp>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <mutex>
@@ -8,122 +7,114 @@
 #include <thread>
 #include <vector>
 
+#include "seed.hpp"
+
+using std::cout;
+using std::endl;
 using std::fstream;
 using std::ios;
+using std::lock_guard;
 using std::move;
+using std::mutex;
 using std::regex;
 using std::regex_replace;
 using std::string;
 using std::thread;
+using std::unique_ptr;
 using std::vector;
-using std::cout;
-using std::mutex;
-using std::lock_guard;
-using std::mutex;
 
-namespace fs = boost::filesystem;
+namespace fs = std::filesystem;
 
 mutex mtx;
 
-inline void safe_out(const string &data)
-{
-    lock_guard<std::mutex> lock(mtx);
-    cout << data << std::endl;
-}
-inline void safe_out(const char *data)
-{
-    lock_guard<std::mutex> lock(mtx);
-    cout << data << std::endl;
+template <typename T>
+void safe_out(const T &data) {
+  lock_guard<mutex> lock(mtx);
+  cout << data << endl;
 }
 
-void process(const string &dir)
-{
-    safe_out("decode: " + dir);
-    fstream infile(dir, ios::in | ios::binary);
-    if (!infile.is_open())
-    {
-        safe_out("qmc file read error");
-        return;
-    }
+void sub_process(const string &dir) {
+  safe_out("decode: " + dir);
+  fstream infile(dir, ios::in | ios::binary);
+  if (!infile.is_open()) {
+    safe_out("qmc file read error");
+    return;
+  }
 
-    string outloc(move(dir));
-    const regex mp3_regex{"\\.(qmc3|qmc0)$"}, flac_regex{"\\.qmcflac$"};
-    auto mp3_outloc = regex_replace(outloc, mp3_regex, ".mp3");
-    auto flac_outloc = regex_replace(outloc, flac_regex, ".flac");
+  string outloc(move(dir));
+  const regex mp3_regex{"\\.(qmc3|qmc0|qmcogg)$"}, flac_regex{"\\.qmcflac$"};
+  auto mp3_outloc = regex_replace(outloc, mp3_regex, ".mp3");
+  auto flac_outloc = regex_replace(outloc, flac_regex, ".flac");
+  outloc = (outloc != mp3_outloc ? mp3_outloc : flac_outloc);
+  auto len = infile.seekg(0, ios::end).tellg();
+  infile.seekg(0, ios::beg);
+  char *buffer = new (std::nothrow) char[len];
+  if (buffer == nullptr) {
+    safe_out("create buffer error");
+    return;
+  }
+  unique_ptr<char[]> auto_delete(buffer);
 
-    assert(mp3_outloc != flac_outloc);
-    outloc = (outloc != mp3_outloc ? mp3_outloc : flac_outloc);
+  infile.read(buffer, len);
+  infile.close();
 
-    auto len = infile.seekg(0, ios::end).tellg();
-    infile.seekg(0, ios::beg);
-    char *buffer = new char[len];
+  qmc_decoder::seed seed_;
+  for (int i = 0; i < len; ++i) {
+    buffer[i] = seed_.next_mask() ^ buffer[i];
+  }
 
-    infile.read(buffer, len);
-    infile.close();
+  fstream outfile(outloc.c_str(), ios::out | ios::binary);
 
-    qmc_decoder::seed seed_;
-    for (int i = 0; i < len; ++i)
-    {
-        buffer[i] = seed_.NextMask() ^ buffer[i];
-    }
-
-    fstream outfile(outloc.c_str(), ios::out | ios::binary);
-
-    if (outfile.is_open())
-    {
-        outfile.write(buffer, len);
-        outfile.close();
-    }
-    else
-    {
-        safe_out("open dump file error");
-    }
-    delete[] buffer;
+  if (outfile.is_open()) {
+    outfile.write(buffer, len);
+    outfile.close();
+  } else {
+    safe_out("open dump file error");
+  }
 }
 
-int main(int argc, char **argv)
-{
-    if (argc > 1)
-    {
-        std::cerr << "put decoder binary file in your qmc file directory, then run it." << std::endl;
-        return 1;
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    std::cerr
+        << "put decoder binary file in your qmc file directory, then run it."
+        << std::endl;
+    return -1;
+  }
+
+  if ((fs::status(fs::path(".")).permissions() & fs::perms::owner_write) ==
+      fs::perms::none) {
+    std::cerr << "please check if you have the write permissions on this dir."
+              << std::endl;
+    return -1;
+  }
+  vector<string> qmc_paths;
+  const regex qmc_regex{"^.+\\.(qmc3|qmc0|qmcflac|qmcogg)$"};
+
+  for (fs::recursive_directory_iterator i{fs::path(".")}, end; i != end; ++i) {
+    auto file_path = i->path().string();
+    if ((fs::status(*i).permissions() & fs::perms::owner_read) !=
+            fs::perms::none &&
+        fs::is_regular_file(*i) && regex_match(file_path, qmc_regex)) {
+      qmc_paths.emplace_back(std::move(file_path));
     }
+  };
 
-    vector<string> qmc_paths;
-    const regex qmc_regex{"^.+\\.(qmc3|qmc0|qmcflac)$"};
+  const auto n_thread = thread::hardware_concurrency();
+  vector<thread> td_group;
 
-    for (fs::recursive_directory_iterator i{fs::path(".")}, end; i != end; ++i)
-    {
-        auto file_path = i->path().string();
-        if (fs::is_regular_file(*i) && regex_match(file_path, qmc_regex))
-        {
-            qmc_paths.emplace_back(file_path);
-        }
-    };
-
-
-    const auto n_thread = thread::hardware_concurrency();
-    vector<thread> td_group;
-
-    for (size_t i = 0; i < n_thread - 1; ++i)
-    {
-        td_group.emplace_back([&qmc_paths, &n_thread](int index) {
-            for (size_t j = index; j < qmc_paths.size(); j += n_thread)
-            {
-                process(qmc_paths[j]);
-            }
+  for (size_t i = 0; i < n_thread - 1; ++i) {
+    td_group.emplace_back(
+        [&qmc_paths, &n_thread](int index) {
+          for (size_t j = index; j < qmc_paths.size(); j += n_thread) {
+            sub_process(qmc_paths[j]);
+          }
         },
-                              i);
-    }
-    for (size_t j = n_thread - 1; j < qmc_paths.size(); j += n_thread)
-    {
-        process(qmc_paths[j]);
-    }
+        i);
+  }
 
-    for (auto &&td : td_group)
-    {
-        td.join();
-    }
+  for (auto &&td : td_group) {
+    td.join();
+  }
 
-    return 0;
+  return 0;
 }
--- src/seed.hpp
+++ src/seed.hpp
@@ -1,60 +1,51 @@
 #ifndef SEED_H
 #define SEED_H
 
+#include <iostream>
 #include <vector>
 
-namespace qmc_decoder
-{
-class seed
-{
-public:
-    seed()
-        : x(-1), y(8), dx(1), index(-1)
-    {
-        seedMap = {{0x4a, 0xd6, 0xca, 0x90, 0x67, 0xf7, 0x52},
-                   {0x5e, 0x95, 0x23, 0x9f, 0x13, 0x11, 0x7e},
-                   {0x47, 0x74, 0x3d, 0x90, 0xaa, 0x3f, 0x51},
-                   {0xc6, 0x09, 0xd5, 0x9f, 0xfa, 0x66, 0xf9},
-                   {0xf3, 0xd6, 0xa1, 0x90, 0xa0, 0xf7, 0xf0},
-                   {0x1d, 0x95, 0xde, 0x9f, 0x84, 0x11, 0xf4},
-                   {0x0e, 0x74, 0xbb, 0x90, 0xbc, 0x3f, 0x92},
-                   {0x00, 0x09, 0x5b, 0x9f, 0x62, 0x66, 0xa1}};
+namespace qmc_decoder {
+class seed {
+ public:
+  seed() : x(-1), y(8), dx(1), index(-1) {
+    seedMap = {{0x4a, 0xd6, 0xca, 0x90, 0x67, 0xf7, 0x52},
+               {0x5e, 0x95, 0x23, 0x9f, 0x13, 0x11, 0x7e},
+               {0x47, 0x74, 0x3d, 0x90, 0xaa, 0x3f, 0x51},
+               {0xc6, 0x09, 0xd5, 0x9f, 0xfa, 0x66, 0xf9},
+               {0xf3, 0xd6, 0xa1, 0x90, 0xa0, 0xf7, 0xf0},
+               {0x1d, 0x95, 0xde, 0x9f, 0x84, 0x11, 0xf4},
+               {0x0e, 0x74, 0xbb, 0x90, 0xbc, 0x3f, 0x92},
+               {0x00, 0x09, 0x5b, 0x9f, 0x62, 0x66, 0xa1}};
+  }
+
+  ~seed() = default;
+  uint8_t next_mask() {
+    uint8_t ret;
+    index++;
+    if (x < 0) {
+      dx = 1;
+      y = (8 - y) % 8;
+      ret = 0xc3;
+    } else if (x > 6) {
+      dx = -1;
+      y = 7 - y;
+      ret = 0xd8;
+    } else {
+      ret = seedMap[y][x];
     }
 
-    ~seed() = default;
-    uint8_t NextMask()
-    {
-        uint8_t ret;
-        index++;
-        if (x < 0)
-        {
-            dx = 1;
-            y = (8 - y) % 8;
-            ret = 0xc3;
-        }
-        else if (x > 6)
-        {
-            dx = -1;
-            y = 7 - y;
-            ret = 0xd8;
-        }
-        else
-        {
-            ret = seedMap[y][x];
-        }
+    x += dx;
+    if (index == 0x8000 || (index > 0x8000 && (index + 1) % 0x8000 == 0))
+      return next_mask();
+    return ret;
+  }
 
-        x += dx;
-        if (index == 0x8000 || (index > 0x8000 && (index + 1) % 0x8000 == 0))
-            return NextMask();
-        return ret;
-    }
-
-private:
-    int x;
-    int y;
-    int dx;
-    int index;
-    std::vector<std::vector<uint8_t>> seedMap;
+ private:
+  int x;
+  int y;
+  int dx;
+  int index;
+  std::vector<std::vector<uint8_t>> seedMap;
 };
-} // namespace qmc_decoder
-#endif // SEED_H
+}  // namespace qmc_decoder
+#endif  // SEED_H
